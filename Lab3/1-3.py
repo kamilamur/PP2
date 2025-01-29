0e36e2bcdc5de8f49853ee7d271667a0f057e710
Kamila <muralinovakamila@gmail.com>
2025-01-29 23:00:50 +0500

    Remove Lab3 folder

--- Lab3/1-3.py
+++
@@ -1,7 +0,0 @@
-class StringManipulator:
-    def _init_(self):
-        self.string = ""
-    def getString(self):
-        self.string = input("Enter a string: ")
-    def printString(self):
-        print(self.string.upper())
--- Lab3/3-3.py
+++
@@ -1,7 +0,0 @@
-class Rectangle(Shape):
-    def _init_(self, length, width):
-        self.length = length
-        self.width = width
-
-    def area(self):
-        return self.length * self.width

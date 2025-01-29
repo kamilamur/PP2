486cb47de7096011400d305733c4eee500a73f44
Kamila <muralinovakamila@gmail.com>
2025-01-29 23:13:03 +0500

    Python Classes

---
+++ Lab3/1-3.py
@@ -0,0 +1,9 @@
+class StringManipulator:
+    def _init_(self):
+        self.string = ""
+    
+    def getString(self):
+        self.string = input("Enter a string: ")
+    
+    def printString(self):
+        print(self.string.upper())

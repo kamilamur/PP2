f042dc923c159b2f9b27c2b53933a529fabcb415
Kamila <muralinovakamila@gmail.com>
2025-02-16 02:22:01 +0500

    LAB 4 tasks wow

--- Lab4/parse_json.py
+++ Lab4/parse_json.py
@@ -3,9 +3,16 @@ with open("sample-data.json", "r") as file:
     data = json.load(file)
 interfaces = data.get("imdata", [])
 print("Interface Status")
-print("=" * 30)
-print("DN")
-print("-" * 30)
+print("=" * 80)
+print(f"{'DN':50} {'Description':15} {'Speed':10} {'MTU':10}")
+print("-" * 80)
+
+
 for item in interfaces:
-    dn_value = item.get("l1PhysIf", {}).get("attributes", {}).get("dn", "N/A")
-    print(dn_value)
+    attributes = item.get("l1PhysIf", {}).get("attributes", {})
+    dn_value = attributes.get("dn", "N/A")
+    description = attributes.get("name", "").strip()  
+    speed = attributes.get("speed", "inherit")  
+    mtu = attributes.get("mtu", "9150")  
+
+    print(f"{dn_value:50} {description:15} {speed:10} {mtu:10}")
